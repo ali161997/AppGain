3b72050e98d0c6f2571c54b43813859270015bb6
ali-hashem-hamed <ali.hashem@advintic.com>
2022-06-12 01:43:45 +0200

    code formatted

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 require('dotenv').config();
-const router = require('./controller/shorten');
+const router = require('../src/controller/shorten');
 const app = express();
 const mongoose = require('mongoose');
 const port = process.env.PORT;
 app.use(express.json());
 app.use('/shortlinks', router);
+app.get('*', (req, res) => {
+  res.send({ message: 'Welcom to Appgain Shortner App' });
+});
 (async () => {
   try {
-    await mongoose.connect(
-      `mongodb://${process.env.DATABASE}/${process.env.dbName}`
-    );
+    //localDB->`mongodb://${process.env.DATABASE}/${process.env.dbName}`
+    await mongoose.connect(process.env.DATABASE);
     app.listen(port, () =>
       console.log(`AppGain ShortenURL App Starts On port ${port}!`)
     );
